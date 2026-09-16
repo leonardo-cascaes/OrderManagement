05be6faf07a3831cfc26dfdbc1aa1a139bf7150a
Leonardo Cascaes <leonardofabiocascaes@gmail.com>
2026-09-16 00:14:41 -0300

    feat: create and validate customer

---
+++ src/OrderManagement.Domain/Entities/Customer.cs
@@ -0,0 +1,25 @@
+﻿using OrderManagement.Domain.Common;
+
+namespace OrderManagement.Domain.Entities
+{
+    public class Customer : Entity
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+
+        public Customer(string name, string email)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Name cannot be empty.", nameof(name));
+
+            if (string.IsNullOrWhiteSpace(email))
+                throw new ArgumentException("Email cannot be empty.", nameof(email));
+
+            if (!email.Contains('@') || !email.Contains('.'))
+                throw new ArgumentException("Invalid email.", nameof(email));
+
+            Name = name;
+            Email = email;
+        }
+    }
+}
---
+++ tests/OrderManagement.Domain.Tests/Entities/Customer/CustomerCreationTests.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using OrderManagement.Domain.Entities;
+
+namespace OrderManagement.Domain.Tests.Entities
+{
+    public class CustomerCreationTests
+    {
+        [Fact]
+        public void Should_create_customer()
+        {
+            // Arrange
+            var name = "Leonardo";
+            var email = "leonardo@email.com";
+
+            // Act
+            var customer = new Customer(name, email);
+
+            // Assert
+            customer.Name.Should().Be(name);
+            customer.Email.Should().Be(email);
+        }
+
+        [Fact]
+        public void Should_not_create_customer_without_name()
+        {
+            // Arrange
+            var email = "leonardo@email.com";
+
+            // Act
+            Action act = () => new Customer(string.Empty, email);
+
+            // Assert
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void Should_not_create_customer_without_email()
+        {
+            // Arrange
+            var name = "Leonardo";
+
+            // Act
+            Action act = () => new Customer(name, string.Empty);
+
+            // Assert
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void Should_not_create_customer_with_invalid_email()
+        {
+            // Arrange
+            var name = "Leonardo";
+            var email = "email-invalido";
+
+            // Act
+            Action act = () => new Customer(name, email);
+
+            // Assert
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
